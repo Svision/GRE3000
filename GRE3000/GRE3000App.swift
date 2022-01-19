698e0885fd4929766590c5e7bc63672a1f3e7fcb
Changhao Song <changhao.song@mail.utoronto.ca>
2022-01-18 21:27:14 -0500

    Initial Commit

---
+++ GRE3000/GRE3000App.swift
@@ -0,0 +1,17 @@
+//
+//  GRE3000App.swift
+//  GRE3000
+//
+//  Created by Changhao Song on 2022-01-18.
+//
+
+import SwiftUI
+
+@main
+struct GRE3000App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
